f95476a7dd78353eadbfb5ce5bef105211a120ef
Alfie <87566534+AlfieRichards@users.noreply.github.com>
2023-01-27 20:27:37 +0000

    Player Movement And Debug System
    I finished the debug system and implemented a rigidbody player movement script. This allows me to move in all dimensions along with crouching and sprinting in a smooth and effective manner.
    
    For the debug system I made it so that it can be opened using keybinds whilst in game, and also added all of the player movement information to it

--- Assets/Scripts/Systems/DebuggingSystem.cs
+++ Assets/Scripts/Systems/DebuggingSystem.cs
@@ -11,65 +11,70 @@ public class DebuggingSystem : MonoBehaviour
 
     //referenced Objects
     private weaponScript _weapon;
-    //private playerMovement _player
+    private playerMovement _player;
 
     private void Start() 
     {
         _weapon = GameObject.Find("WeaponsSystem").GetComponent<weaponScript>();
-        //_player = GameObject.Find("Player").GetComponent<playerMovement>();
+        _player = GameObject.Find("Player").GetComponent<playerMovement>();
     }
     
     void OnGUI() 
     {
-        GUI.Box(new Rect(10,10,170,130), "Debugging Controls");
-
-        if (GUI.Button(new Rect(20,40,150,20), "Show Weapon Values"))
+        if(_player._debugGUI)
         {
-            Debug.Log("Showing Weapon Values!");
+            GUI.Box(new Rect(10,10,170,130), "Debugging Controls");
+            if (GUI.Button(new Rect(20,40,150,20), "Show Weapon Values"))
+            {
+                Debug.Log("Showing Weapon Values!");
+                if(_showWeapons)
+                {
+                    _showWeapons = false;
+                }
+                else
+                {
+                    _showWeapons = true;
+                }
+            }
+
+            if (GUI.Button(new Rect(20,70,150,20), "Show Player Values"))
+            {
+                Debug.Log("Showing Player Values!");
+                if(_showPlayer)
+                {
+                    _showPlayer = false;
+                }
+                else
+                {
+                    _showPlayer = true;
+                }
+            }
+
             if(_showWeapons)
             {
-                _showWeapons = false;
-            }
-            else
-            {
-                _showWeapons = true;
-            }
-        }
+                GUI.Box(new Rect (Screen.width - 250,0,250,200), "Weapon Values");
+                GUI.Label(new Rect(Screen.width - 230, 20, 100, 20), "Ammunition");
+                GUI.Label(new Rect(Screen.width - 230, 40, 100, 20), "Mag Size: " + _weapon._magSize);
+                GUI.Label(new Rect(Screen.width - 230, 60, 100, 20), "Ammo In Mag: " + _weapon._ammoCapacity);
+                GUI.Label(new Rect(Screen.width - 230, 80, 100, 20), "Spare Ammo: " + _weapon._spareAmmo);
 
-        if (GUI.Button(new Rect(20,70,150,20), "Show Player Values"))
-        {
-            Debug.Log("Showing Player Values!");
+                GUI.Label(new Rect(Screen.width - 230, 100, 250, 20), "Booleans");
+                GUI.Label(new Rect(Screen.width - 230, 120, 250, 20), "Firing: " + _weapon._firing);
+                GUI.Label(new Rect(Screen.width - 230, 140, 250, 20), "Can Fire: " + _weapon._canFire);
+                GUI.Label(new Rect(Screen.width - 230, 160, 250, 20), "Can Reload: " + _weapon._canReload);
+                GUI.Label(new Rect(Screen.width - 230, 180, 250, 20), "Reloading: " + _weapon._reloading);
+            }
+
             if(_showPlayer)
             {
-                _showPlayer = false;
-            }
-            else
-            {
-                _showPlayer = true;
-            }
-        }
-
-        if(_showWeapons)
-        {
-            GUI.Box(new Rect (Screen.width - 250,0,250,110), "Weapon Values");
-            GUI.Label(new Rect(Screen.width - 230, 20, 100, 20), "Ammunition");
-            GUI.Label(new Rect(Screen.width - 230, 40, 100, 20), "Mag Size: " + _weapon._magSize);
-            GUI.Label(new Rect(Screen.width - 230, 60, 100, 20), "Ammo In Mag: " + _weapon._ammoCapacity);
-            GUI.Label(new Rect(Screen.width - 230, 80, 100, 20), "Spare Ammo: " + _weapon._spareAmmo);
-
-            GUI.Label(new Rect(Screen.width - 230, 100, 100, 20), "Booleans");
-            GUI.Label(new Rect(Screen.width - 230, 120, 100, 20), "Firing: " + _weapon._firing);
-            GUI.Label(new Rect(Screen.width - 230, 140, 100, 20), "Can Fire: " + _weapon._canFire);
-            GUI.Label(new Rect(Screen.width - 230, 160, 100, 20), "Can Reload: " + _weapon._canReload);
-            GUI.Label(new Rect(Screen.width - 230, 180, 100, 20), "Reloading: " + _weapon._reloading);
-        }
-        if(_showPlayer)
-        {
-            // GUI.Box(new Rect (Screen.width - 500,0,250,110), "Player");
-            // GUI.Label(new Rect(Screen.width - 480, 20, 100, 20), "XYZ: " + pitch);
-            // GUI.Label(new Rect(Screen.width - 480, 40, 100, 20), "Grounded: " + roll);
-            // GUI.Label(new Rect(Screen.width - 480, 60, 100, 20), "Crouching: " + yaw);
-            // GUI.Label(new Rect(Screen.width - 480, 80, 100, 20), "Sprinting: " + throttle);
+                GUI.Box(new Rect (Screen.width - 500,0,250,140), "Player");
+                GUI.Label(new Rect(Screen.width - 480, 20, 250, 20), "XYZ: " + _player._location);
+                GUI.Label(new Rect(Screen.width - 480, 40, 250, 20), "Grounded: " + _player._grounded);
+                GUI.Label(new Rect(Screen.width - 480, 60, 250, 20), "Crouching: " + _player._crouching);
+                GUI.Label(new Rect(Screen.width - 480, 80, 250, 20), "Sprinting: " + _player._sprinting);
+                GUI.Label(new Rect(Screen.width - 480, 100, 250, 20), "xRotation: " + _player._xRotation);
+                GUI.Label(new Rect(Screen.width - 480, 120, 250, 20), "yRotation: " + _player._yRotation);
+            }    
         }
     }
 }
--- Assets/playerMovement.cs
+++ Assets/playerMovement.cs
@@ -7,16 +7,201 @@ public class playerMovement : MonoBehaviour
 
     //need walking sprinting jumping and crouching
 
+    //movementOptions
+    [Header("Movement Options")]
+    [SerializeField] private float _speed = 5;
+    [SerializeField] private float _sprintSpeed = 10;
+    [SerializeField] private float _crouchSpeed = 0;
+
+    //jumpOptions
+    [Header("Jumping Options")]
+    [SerializeField] private float _jumpHeight;
+    [SerializeField] private float _jumpPower;
+
+    //mouseOptions
+    [Header("Mouse Options")]
+    [SerializeField] public float mouseSensitivity = 100f;
+
+    //objects
+    [Header("Objects")]
+    [SerializeField] private Transform _groundPoint;
+    [SerializeField] private LayerMask _ground;
+    [SerializeField] private Transform _playerBody;
+    [SerializeField] private Transform _camParentBone;
+    [HideInInspector] private Rigidbody _rb;
+
+    //debuggingBools
+    [HideInInspector] public bool _crouching = false;
+    [HideInInspector] public bool _sprinting = false;
+    [HideInInspector] public bool _grounded;
+    [HideInInspector] public Vector3 _location;
+    [HideInInspector] public float _yRotation = 0f;
+    [HideInInspector] public float _xRotation = 0f;
+    [HideInInspector] public bool _locked = true;
+    [HideInInspector] public bool _debugGUI = false;
+    private bool _recentDebugOption = false;
+
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate() 
+    {
+        MouseRotation();
+        CheckInput();
+        RigidbodyMovement();
+        DebugCursor();
+
+        _location = transform.position;
+    }
+
+    void CheckInput()
+    {
+        if(Input.GetKey(KeyCode.LeftShift)){_sprinting = true;}else{_sprinting = false;}
+        if(Input.GetKey(KeyCode.LeftControl)){_crouching = true;}else{_crouching = false;}
+    }
+
+    void RigidbodyMovement()
+    {
+        Vector3 _tempVelocity;
+        float _tSpeed = _speed;
+
+        //gets player input
+        float _vertical = Input.GetAxisRaw("Vertical");
+        float _horizontal = Input.GetAxisRaw("Horizontal");
+
+        //if youre crouched and try to moves plays the uncrouch anim, cant move until uncrouched
+        if(Input.GetAxisRaw("Vertical") != 0 || Input.GetAxisRaw("Horizontal") != 0)
+        {
+            if(_crouching)
+            {
+                _tempVelocity = new Vector3(0,_rb.velocity.y,0);
+                _rb.velocity = _tempVelocity;
+                ToggleCrouch();
+                return;
+            }
+        }
+
+        //if sprinting make move faster
+        if(_sprinting)
+        {
+            _tSpeed = _sprintSpeed;
+        }
+
+        //handle movement anims
+        if(_vertical != 0)
+        {
+            //anim.setBool walking = true
+        }
+        else
+        {
+            if(_horizontal != 0)
+            {
+                //anim.setBool sideWalking = true
+            }
+        }
+
+        //sets velocity
+        _tempVelocity = (transform.forward * _vertical * _tSpeed) + (transform.right * _horizontal * _tSpeed);
+        _rb.velocity = _tempVelocity;
+    }
+
+    void MouseRotation()
+    {
+        //gets mouse input
+        float _mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float _mouseY = -Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        //stops rot from getting infinitely higher and going out of the limits of a float
+        if(_xRotation > 360 || _xRotation < -360)
+        {
+            _xRotation = 0;
+        }
+
+        //constrains y input
+        _yRotation = _yRotation += _mouseY;
+        _yRotation = Mathf.Clamp(_yRotation, -75f, 75f);
+
+        _xRotation = _xRotation += _mouseX;
+
+        //rotates player horizontally (y axis)
+        _playerBody.transform.rotation = Quaternion.Euler(0, _xRotation, 0);
+
+        //rotates player bone vertically (x axis)
+        _camParentBone.transform.rotation = Quaternion.Euler(_yRotation, _xRotation, 0);
+    }
+
+
+
+
+    void Jump()
     {
         
+    }
+
+    void ToggleCrouch()
+    {
+        if(_crouching){
+            //anim.setbool crouching false
+            _crouching = false;
+        }
+    }
+
+    //call this whenever the animation event for crouching is called (event at end of anim)
+    void UnCrouch()
+    {
+        _crouching = false;
+    }
+
+    void DebugCursor()
+    {
+        if(Input.GetKey(KeyCode.LeftControl) && Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.LeftAlt))
+        {
+            if(Input.GetKey("h") && _locked && !_recentDebugOption)
+            {
+                Debug.Log("unlocked");
+                Cursor.lockState = CursorLockMode.None;
+                _locked = false;
+                _recentDebugOption = true;
+                Invoke("ResetDebugCursor", 0.1f);
+                return;
+            }
+            
+            if(Input.GetKey("h") && !_locked && !_recentDebugOption)
+            {
+                Debug.Log("locked");
+                Cursor.lockState = CursorLockMode.Locked;
+                _locked = true;
+                _recentDebugOption = true;
+                Invoke("ResetDebugCursor", 0.1f);
+            }
+
+            if(Input.GetKey("j") && !_locked && !_recentDebugOption)
+            {
+                Debug.Log("DebugGuiOpen");
+                _debugGUI = true;
+                _recentDebugOption = true;
+                Invoke("ResetDebugCursor", 0.1f);
+                return;
+            }
+
+            if(Input.GetKey("j") && _debugGUI && !_recentDebugOption)
+            {
+                Debug.Log("DebugGuiClosed");
+                _debugGUI = false;
+                _recentDebugOption = true;
+                Invoke("ResetDebugCursor", 0.1f);
+            }
+        }
+    }
+
+    void ResetDebugCursor()
+    {
+        _recentDebugOption = false;
     }
 }
