0406c3f7967a2ac64b033d6d2e0e95c9ce098f8e
Alfie <87566534+AlfieRichards@users.noreply.github.com>
2023-05-03 22:43:46 +0100

    Target changes
    After discovering targets would not swing in builds i began researching why.
    
    In conclusion im still unsure however i narrowed it down to my method of moving them being to adjust the anchor point. Though this shouldve worked it for some reason doesent in builds which im assuming is a lack of documentation on unitys part. After further digging and experimenting I created a system using 3 hinge joints instead of 2, these are moved by moving the top one with rigidbody move.
    
    This solves literally all of the problems and is amazing

--- Assets/Scripts/TargetController.cs
+++ Assets/Scripts/TargetController.cs
@@ -7,25 +7,37 @@ public class TargetController : MonoBehaviour
     [SerializeField] private Transform _targetHolder;
     [SerializeField] private HingeJoint _targetAnchor;
     [HideInInspector] private float _amplification = 1;
-    [HideInInspector] private float _z;
-    [HideInInspector] private float _zDifference;
     [HideInInspector] private Vector3 _anchorIntended;
+    Rigidbody retard;
+    public GameObject twat;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        retard = twat.GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float _zDifference = _targetHolder.position.z - twat.transform.position.z;
+        float _z = twat.transform.position.z + (_zDifference * _amplification);
+        _anchorIntended = new Vector3(twat.transform.position.x, twat.transform.position.y, _z);
+
+        retard.MovePosition(_anchorIntended);
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        _zDifference = _targetHolder.position.z - _targetAnchor.connectedAnchor.z;
-        _z = _targetAnchor.connectedAnchor.z + (_zDifference * _amplification);
-        _anchorIntended = new Vector3(_targetAnchor.connectedAnchor.x, _targetAnchor.connectedAnchor.y, _z);
-        _targetAnchor.connectedAnchor = _anchorIntended;
+    // void Update()
+    // {
+    //     _zDifference = _targetHolder.position.z - _targetAnchor.connectedAnchor.z;
+    //     _z = _targetAnchor.connectedAnchor.z + (_zDifference * _amplification);
+    //     _anchorIntended = new Vector3(_targetAnchor.connectedAnchor.x, _targetAnchor.connectedAnchor.y, _z);
+    //     _targetAnchor.connectedAnchor = _anchorIntended;
 
-        _targetAnchor.autoConfigureConnectedAnchor = true;
-        _targetAnchor.autoConfigureConnectedAnchor = false;
-    }
+    //     Debug.Log(_targetAnchor.connectedAnchor);
+    //     //Debug.Log(_anchorIntended);
+
+    //     _targetAnchor.autoConfigureConnectedAnchor = true;
+    //     _targetAnchor.autoConfigureConnectedAnchor = false;
+    // }
 }
