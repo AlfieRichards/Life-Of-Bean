1107fd28cf60e7e682259b0fcbe8293987c58c00
Alfie <87566534+AlfieRichards@users.noreply.github.com>
2023-05-11 16:09:16 +0100

    Video Settings Done

--- Assets/mainMenuController.cs
+++ Assets/mainMenuController.cs
@@ -16,11 +16,21 @@ public class mainMenuController : MonoBehaviour
     public GameObject subMenu;
     public GameObject mainMenu;
 
+    public GameObject videoSettings;
+    public GameObject audioSettings;
+
     int index = 0;
 
     bool subMenuBool = false;
     bool vidOpen = false;
     bool audOpen = false;
+
+    bool menusVisible = false;
+    public CanvasGroup canvas;
+
+    public  float lerp = 0f, duration = 2f;
+
+    
     
     // Start is called before the first frame update
     void Start()
@@ -31,13 +41,35 @@ public class mainMenuController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(menusVisible)
+        {
+            if(canvas.alpha < 1)
+            {
+                lerp += Time.deltaTime / duration;
+                canvas.alpha = (int)Mathf.Lerp (canvas.alpha, 1f, lerp);
+                //canvas.alpha += 0.05f;
+            }
+        }
+        else
+        {
+            if(canvas.alpha > 0)
+            {
+                lerp -= Time.deltaTime / duration;
+                canvas.alpha = (int)Mathf.Lerp (canvas.alpha, 0f, lerp * -1.5f);
+                //canvas.alpha -= 0.05f;
+            }
+        }
+
+        canvas.alpha = lerp;
+
         if(Input.GetKeyDown(KeyCode.Return))
         {
-            Debug.Log("asdgksdfghjsdf");
             if(index == 1 && !subMenuBool)
             {
                 subMenuBool = true;
                 index = 0;
+                lerp = 0;
+                canvas.alpha = 0;
                 
                 mainMenu.SetActive(false);
                 subMenu.SetActive(true);
@@ -45,32 +77,62 @@ public class mainMenuController : MonoBehaviour
 
             if(index == 0 && subMenuBool)
             {
-                if(!audOpen && !vidOpen){GetComponent<AnimationManager>().PlayNoSkip("VideoOpen");}
-                if(audOpen && !vidOpen)
+                if(!audOpen && !vidOpen)
                 {
-                    GetComponent<AnimationManager>().PlayNoSkip("AudioClose");
                     GetComponent<AnimationManager>().PlayNoSkip("VideoOpen");
+
+                    audOpen = false;
+                    vidOpen = true;
+
+                    Invoke("VideoSettingsActive", 2.2f);
                 }
-                audOpen = false;
-                vidOpen = true;
+                else
+                {
+                    if(audOpen && !vidOpen)
+                    {
+                        menusVisible = false;
+                        GetComponent<AnimationManager>().PlayNoSkip("AudioClose");
+                        GetComponent<AnimationManager>().PlayNoSkip("VideoOpen");
+                        //GetComponent<AnimationManager>().PlayNoSkip("FadeIn");
+                    }
+                    audOpen = false;
+                    vidOpen = true;
+
+                    Invoke("VideoSettingsActive", 4.2f);
+                }
                 //video settings
             }
 
             if(index == 1 && subMenuBool)
             {
-                if(!audOpen && !vidOpen){GetComponent<AnimationManager>().PlayNoSkip("AudioOpen");}
-                if(!audOpen && vidOpen)
+                if(!audOpen && !vidOpen)
                 {
-                    GetComponent<AnimationManager>().PlayNoSkip("VideoClose");
                     GetComponent<AnimationManager>().PlayNoSkip("AudioOpen");
+
+                    vidOpen = false;
+                    audOpen = true;
+
+                    Invoke("AudioSettingsActive", 2.2f);
                 }
-                vidOpen = false;
-                audOpen = true;
+                else
+                {
+                    if(!audOpen && vidOpen)
+                    {
+                        menusVisible = false;
+                        GetComponent<AnimationManager>().PlayNoSkip("VideoClose");
+                        GetComponent<AnimationManager>().PlayNoSkip("AudioOpen");
+                    }
+                    vidOpen = false;
+                    audOpen = true;
+
+                    Invoke("AudioSettingsActive", 4.2f);
+                }
                 //audio settings
             }
 
             if(index == 2 && subMenuBool)
             {
+                menusVisible = false;
                 if(audOpen){GetComponent<AnimationManager>().PlayNoSkip("AudioClose");}
                 if(vidOpen){GetComponent<AnimationManager>().PlayNoSkip("VideoClose");}
 
@@ -79,6 +141,8 @@ public class mainMenuController : MonoBehaviour
 
                 vidOpen = false;
                 audOpen = false;
+
+                Invoke("NoSettingsActive", 3f);
 
                 mainMenu.SetActive(true);
                 subMenu.SetActive(false);
@@ -134,6 +198,30 @@ public class mainMenuController : MonoBehaviour
             }
             default:
                 break;
+        }
+    }
+
+    void VideoSettingsActive()
+    {
+        Invoke("MenusVisible", 1f);
+    }
+
+    void AudioSettingsActive()
+    {
+        Invoke("MenusVisible", 1f);
+    }
+
+    void NoSettingsActive()
+    {
+        Invoke("MenusVisible", 1f);
+    }
+
+    void MenusVisible()
+    {
+        if(vidOpen)
+        {
+            lerp = 0;
+            menusVisible = true;
         }
     }
 }
