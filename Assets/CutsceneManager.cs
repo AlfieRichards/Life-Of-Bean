99dbeb712bafc7c6f787233ca2708142d78c4d1d
Alfie <87566534+AlfieRichards@users.noreply.github.com>
2023-05-19 15:17:01 +0100

    Fixed issues
    Fixed cutscene not fitting correctly
    Fixed there being no cutscene volume control
    Fixed cutscene playing every time you open the game

--- Assets/CutsceneManager.cs
+++ Assets/CutsceneManager.cs
@@ -1,15 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.Video;
 
 public class CutsceneManager : MonoBehaviour
 {
     [SerializeField] private LevelLoader levelLoader;
+    public AudioMixer masterMixer;
+    public Slider cutscene;
+    public VideoPlayer videoPlayer;
+    int cutScenePlayed;
 
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(PlayCredits());
+        LoadPrefs();
+        StartCoroutine(VideoDelay());
+
+        if(cutScenePlayed == 0)
+        {
+            videoPlayer.Play();
+            PlayerPrefs.SetInt("CutScenePlayed", 1);
+        }
+        else
+        {
+            levelLoader.StartCoroutine(levelLoader.LoadLevel(1));
+        }
     }
 
 
@@ -21,13 +39,26 @@ public class CutsceneManager : MonoBehaviour
         }
     }
 
+    public void SetCutSceneVolume()
+    {
+        masterMixer.SetFloat("CutSceneVolume", cutscene.value);
+        PlayerPrefs.SetFloat("CutSceneVolume", cutscene.value);
+        Debug.Log(cutscene.value);
+    }
 
-    public IEnumerator PlayCredits()
+
+    public IEnumerator VideoDelay()
     {
         //wait
         yield return new WaitForSeconds(55);
 
         //load menu
         levelLoader.StartCoroutine(levelLoader.LoadLevel(1));
+    }
+
+    void LoadPrefs()
+    {
+        cutscene.value = PlayerPrefs.GetFloat("CutSceneVolume", -10);
+        cutScenePlayed = PlayerPrefs.GetInt("CutScenePlayed", 0);
     }
 }
